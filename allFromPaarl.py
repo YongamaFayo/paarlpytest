db0a2c0ddaa8146e67ee11ad66173e3575021174
Yongama Patrick Fayo <yongama.fayo@wundermanthompson.com>
2021-08-19 11:26:08 +0200

    first commit

---
+++ allFromPaarl.py
@@ -0,0 +1,12 @@
+
+# def allFromPaarl(numbers, symbol):
+numbers = input("numbers:")
+symbol = input("symbol:")
+Registration = symbol+numbers
+if symbol.upper() == "CY":
+    selected = numbers
+    print("It is from Paarl")
+else:
+    converted = numbers
+
+    print("Not a paarl reg")
---
+++ test_allFromPaarl.py
@@ -0,0 +1,5 @@
+import allFromPaarl
+import pytest
+
+def test_carReg():
+    assert allFromPaarl.allFromPaarl(CY123) == 'It is from Paarl'
